ca53f25e2a6a9cc4443a0d73bb78a3600b66e729
kayode <kayode.richards@gmail.com>
2020-10-09 12:26:57 -0400

    finished character

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {<StarCharacters key={starData.id} characters={starData}/>}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,7 +3,22 @@ import React from 'react'
 
 export default function StarCharacter(props){
     const {characters}=props
+    console.log(props)
     return (
-        
+        <>
+        {characters.map(character=>{
+            return(
+                <>
+                <h1>Name: {character.name}</h1>
+                <p>Gender: {character.gender.toUpperCase()}</p>
+                <p>Eye Color: {character.eye_color.toUpperCase()}</p>
+                <p>Birth Year: {character.birth_year}</p>
+                <p>Hair Color: {character.hair_color.toUpperCase()}</p>
+                <a href={character.starships ? `${character.starships[0]}` : null}> {character.starships.length > 0 ? `${character.name}'s StarShip One` : 'No StarShips here'}</a>
+                <a href={character.starships ? `${character.starships[1]}` : null}> {character.starships.length > 0 ? `${character.name}'s StarShip Two` : ' '}</a>
+                </>
+            )
+        })}
+        </>
     )
 }
